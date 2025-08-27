ab55eeeacf11d4e37bfffab77baede74ed6822ad
Eduarda de Souza <95228085+madusouzz@users.noreply.github.com>
2025-08-27 10:16:59 -0300

    Delete Documents/dev/talismã directory

--- Documents/dev/talismã/js/script.js
+++
@@ -1,4 +0,0 @@
-// Pega o elemento com o ID 'current-year'
-        const yearElement = document.getElementById('current-year');
-        // Pega o ano atual e o define como o texto do elemento
-        yearElement.textContent = new Date().getFullYear();
